41ef22d32c1b97e53f99f41862266187455a370e
Lars Busk <lab@foss.dk>
2017-10-18 23:09:37 +0200

    AQdded Primes

---
+++ Primes/Primes/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Primes
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnFindPrimes = new System.Windows.Forms.Button();
+      this.tbMaxNumber = new System.Windows.Forms.TextBox();
+      this.rtbNumbers = new System.Windows.Forms.RichTextBox();
+      this.SuspendLayout();
+      // 
+      // btnFindPrimes
+      // 
+      this.btnFindPrimes.Location = new System.Drawing.Point(371, 316);
+      this.btnFindPrimes.Name = "btnFindPrimes";
+      this.btnFindPrimes.Size = new System.Drawing.Size(75, 23);
+      this.btnFindPrimes.TabIndex = 0;
+      this.btnFindPrimes.Text = "Find tal";
+      this.btnFindPrimes.UseVisualStyleBackColor = true;
+      this.btnFindPrimes.Click += new System.EventHandler(this.btnFindPrimes_Click);
+      // 
+      // tbMaxNumber
+      // 
+      this.tbMaxNumber.Location = new System.Drawing.Point(346, 279);
+      this.tbMaxNumber.Name = "tbMaxNumber";
+      this.tbMaxNumber.Size = new System.Drawing.Size(100, 20);
+      this.tbMaxNumber.TabIndex = 1;
+      // 
+      // rtbNumbers
+      // 
+      this.rtbNumbers.Location = new System.Drawing.Point(16, 15);
+      this.rtbNumbers.Name = "rtbNumbers";
+      this.rtbNumbers.Size = new System.Drawing.Size(313, 324);
+      this.rtbNumbers.TabIndex = 2;
+      this.rtbNumbers.Text = "";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(458, 351);
+      this.Controls.Add(this.rtbNumbers);
+      this.Controls.Add(this.tbMaxNumber);
+      this.Controls.Add(this.btnFindPrimes);
+      this.Name = "Form1";
+      this.Text = "Primtal";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnFindPrimes;
+    private System.Windows.Forms.TextBox tbMaxNumber;
+    private System.Windows.Forms.RichTextBox rtbNumbers;
+  }
+}
+
---
+++ Primes/Primes/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primes
+{
+  public partial class Form1 : Form
+  {
+    List<int> primes = new List<int>() { 2, 3 };
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnFindPrimes_Click(object sender, EventArgs e)
+    {
+      
+
+      //IEnumerable<int> nums = Enumerable.Range(2, maxNumber - 1);
+      
+
+      new Thread(new ThreadStart(FindPrimes)).Start();
+
+     // PrintNums(primes);
+    }
+
+    private void FindPrimes()
+    {
+      int maxNumber = int.Parse(tbMaxNumber.Text);
+      
+      for (int i = 3; i < maxNumber; i++)
+      {
+        if (primes.All<int>(r => i % r != 0 && r != 1))
+        {
+          primes.Add(i);
+          //rtbNumbers.AppendText(i.ToString() + ", ");
+        }
+      }
+
+      PrintNums(primes);
+    }
+
+    private void PrintNums(IEnumerable<int> nums)
+    {
+      rtbNumbers.Clear();
+      foreach (int num in nums)
+      {
+        rtbNumbers.AppendText(num.ToString() + ", ");
+      }
+    }
+  }
+}
